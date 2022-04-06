af8c5680828b70e3678d27bea1440028c4f058fb
revv-tech <52094428+revv-tech@users.noreply.github.com>
2022-04-05 20:10:36 -0600

    Round Robin Done

--- src/main.c
+++ src/main.c
@@ -6,12 +6,13 @@ void roundRobin(int n, int qt, int bt[10]) {
     int tat[10];
     // Waiting Time
     int wt[10];
+    // Remaining Burst Time
+    int rem_bt[10];
     // Counter
     int count = 0;
     // SQ
     int sq = 0;
-    // Rem Burst Time
-    int rem_bt[10];
+    
     // Number of proccesses for and burst copying
     for (i = 0; i < n ; i++){
         
@@ -20,27 +21,29 @@ void roundRobin(int n, int qt, int bt[10]) {
 
     while (1) {
 
-        for (i = 0, count = 0; i < n ; i++) {
+        for (i = 0, count = 0; i < n ; i++) 
+        {
             
             temp = qt;
 
-            if (rem_bt[i] == 0){
+            if (rem_bt[i] == 0)
+            {
                 count++;
                 continue;
             }
 
-            if (rem_bt[i] > qt){
+            if (rem_bt[i] > qt)
                 rem_bt[i] = rem_bt[i]- qt;
-            }
-            else {
-                if (rem_bt[i] >= 0){
+            else 
+                if (rem_bt[i] >= 0)
+                {
                     temp = rem_bt[i];
                     rem_bt[i] = 0;
                 }
                 sq = sq + temp;
                 tat[i] = sq;
 
-            }
+            
         }
         if (n == count) 
             break;
@@ -50,8 +53,9 @@ void roundRobin(int n, int qt, int bt[10]) {
 
     for ( i = 0; i < n ; i++){
         wt[i] = tat[i] - bt[i];
-        printf("\n%d\t\t\t%d\t\t\t%d\t\t\t%d", i + 1,bt[i],tat[i],wt[i]);
+        printf("\n%d\t\t\t%d\t\t\t\t%d\t\t\t%d", i + 1,bt[i],tat[i],wt[i]);
     }
+    
 }
 int main() {
     int bt[10] = {5,3,1,4};
