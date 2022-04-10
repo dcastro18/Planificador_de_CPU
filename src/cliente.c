27a531d7aa131c526976e9946701d28bb2bafa1a
David Castro <h.davidcastro14@gmail.com>
2022-04-10 01:24:27 -0600

    Socket conection

--- src/cliente.c
+++ src/cliente.c
@@ -5,6 +5,28 @@
 #include <string.h>
 #include <time.h>
 
+//Para Sockets
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include<arpa/inet.h>
+
+
+// VARIABLES GLOBALES
+int sockfd;
+int len;
+struct sockaddr_in address;
+int result;
+char ch[1024];
+char c[1024];
+int buf;
+int inicio = 0;
+char cs[1024];
+int bufs;
+int ciclo =1;
+char ipserver[16] = "127.0.0.1";
+int puerto = 5000;
+
 
 
 int randNumber(int min, int max) {
@@ -16,9 +38,34 @@ int randNumber(int min, int max) {
 
 void *sendData (void *args) {
     char *msg = (char *)args;
-    printf("%s",msg);
     // enviar msg por el socket
 
+    sockfd = socket(AF_INET, SOCK_STREAM,0);
+    //llenado de la estructura de datos
+    address.sin_family = AF_INET;
+    address.sin_addr.s_addr = inet_addr(ipserver);
+    address.sin_port = puerto;
+    len = sizeof(address);
+
+    //conectar con el server
+    result = connect(sockfd, (struct sockaddr *)&address, len);
+    if(result ==-1){
+        perror("ERROR EN LA CONEXION\n");
+        close(sockfd);
+    }
+
+    //validar el inicio de sesion
+    if(inicio==0){
+        printf("--------------- SESION INICIADA --------------\n");
+        recv(sockfd, ch, 1024,0);
+        printf("%s\n",ch);
+        inicio = 1;
+    }
+    send(sockfd,msg, 1024,0);
+
+    recv(sockfd, c, 1024,0);
+    printf("Proceso recibido! El PID asignado es el número: %s\n",c);
+    close(sockfd);
     // matar el thread
 }
 
@@ -41,31 +88,32 @@ void *readFile (void *args) {
     char con[fileSize]; // variable to read the content
     int i = 0;
 
+    pthread_t pthread;
+
     while (fgets(con,fileSize, fp)!=NULL) { // reading file content
 
         if(i != 0)
         {
             //Espera dos segundos y y envia al server
             sleep(2);
-            pthread_t pthread;
+
             pthread_create(&pthread,NULL, sendData, (void *)&con);
             pthread_join(pthread,NULL);
 
-            // Recibo el PID y creo el hilo con ese ID (y con la info)
-            char PID[] = "1";
-            printf("El PID es %s\n",PID);
-            int randNum = randNumber(3,8);
-            sleep(randNum);
+            //int randNum = randNumber(3,8);
+            //sleep(randNum);
         }
         i++;
     }
+
     fclose(fp); // closing file
-
-
+    pthread_exit(&pthread);
 }
 
 int main(int argc, char const *argv[])
 {
+
+    pthread_t mainThread;
     int n, opcion;
 
     do
@@ -74,13 +122,13 @@ int main(int argc, char const *argv[])
         printf( "\n   1. Cliente manual." );
         printf( "\n   2. Cliente automático");
         printf( "\n   3. Salir." );
-        printf( "\n\n   Introduzca opción que desee (1-3): ");
+        printf( "\n\n  Introduzca opción que desee (1-3): ");
 
         scanf( "%d", &opcion );
 
         /* Inicio del anidamiento */
 
-        pthread_t mainThread;
+
 
         // crear el socket
 
